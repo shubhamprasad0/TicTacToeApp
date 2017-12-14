7798c66a421d39205df6dbe235d7e7acbf14a209
Shubham Prasad <shubhamprasad63@gmail.com>
2017-12-14 12:30:11 +0530

    Layout improved, each RadioButton put inside RadioGroup

--- app/src/main/java/com/example/shubham/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/shubham/tictactoe/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.shubham.tictactoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Method called when the user clicks on the play button
+     */
+    fun startGame(view: View) {
+        Toast.makeText(this, "The button is working", Toast.LENGTH_SHORT).show()
     }
 }
