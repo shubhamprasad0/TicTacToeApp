bebf981d3bb95cdac42bbad9dbb6e08095a8fdbe
Shubham Prasad <shubhamprasad63@gmail.com>
2017-12-14 13:42:30 +0530

    Radio Buttons and Play button working, take user to different activities depending upon the choice

--- app/src/main/java/com/example/shubham/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/shubham/tictactoe/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.shubham.tictactoe
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +17,35 @@ class MainActivity : AppCompatActivity() {
 
     /**
      * Method called when the user clicks on the play button
+     *
+     * Starts the game in single-player mode if the user chose "Play with Computer"
+     * Starts the game in two-player mode if the user chose "Play with Human"
+     *
      */
     fun startGame(view: View) {
-        Toast.makeText(this, "The button is working", Toast.LENGTH_SHORT).show()
+        val index = getIndexOfCheckedRadioButton()
+        if (index == 0) {
+            val intent = Intent(this, PlayWithComputerActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(this, PlayWithHumanActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    /**
+     * Finds which RadioButton was checked by the user and returns appropriate index:-
+     *      0. Play with Computer
+     *      1. Play with Human
+     *
+     * @return index (index of the checked RadioButton in the RadioGroup
+     */
+    private fun getIndexOfCheckedRadioButton(): Int {
+        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+        val checkedRadioButtonId = radioGroup.checkedRadioButtonId
+        val checkedRadioButton = findViewById<RadioButton>(checkedRadioButtonId)
+        val index = radioGroup.indexOfChild(checkedRadioButton)
+        return index
     }
 }
---
+++ app/src/main/java/com/example/shubham/tictactoe/PlayWithHumanActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shubham.tictactoe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayWithHumanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_with_human)
+    }
+}
